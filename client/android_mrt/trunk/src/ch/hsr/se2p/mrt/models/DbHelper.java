8ac3ef786d166400dd4769e86aedc9210eaa7cf5
Lukas Elmer <lukas.elmer@gmail.com>
2011-04-05 07:48:52 +0000

    refs #275

--- client/android_mrt/trunk/src/ch/hsr/se2p/mrt/models/DbHelper.java
+++ client/android_mrt/trunk/src/ch/hsr/se2p/mrt/models/DbHelper.java
@@ -4,48 +4,117 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
+import android.util.Log;
 import ch.hsr.se2p.mrt.util.Config;
 
-public class DbHelper extends SQLiteOpenHelper {
+public class DbHelper {
 
+	// Customers table
+	// public static final String CUSTOMERS_TABLE_NAME = "customers";
+	// public static final String[] CUSTOMERS_C_FIRST_NAME = { "first_name", TEXT };
+	// public static final String[] CUSTOMERS_C_LAST_NAME = { "last_name", TEXT };
+	// public static final String[] CUSTOMERS_C_PHONE = { "phone", TEXT };
+	// public static final String[] CUSTOMERS_C_POSITION = { "position", TEXT };
+	// public static final String[] CUSTOMERS_C_UPDATED_AT = { "updated_at", INTEGER };
+	// public static final String[] CUSTOMERS_C_ADDRESS_ID = { "address_id", INTEGER };
+	// public static final String[][] CUSTOMERS_COLUMNS = { CUSTOMERS_C_FIRST_NAME, CUSTOMERS_C_LAST_NAME, CUSTOMERS_C_PHONE, CUSTOMERS_C_POSITION, CUSTOMERS_C_UPDATED_AT, CUSTOMERS_C_ADDRESS_ID };
+
+	// Time entry table
+	// public static final String TIME_ENTRY_TYPES_TABLE_NAME = "time_entry_types";
+	// public static final String[] TIME_ENTRY_TYPES_C_FIRST_NAME = { "description", TEXT };
+	// public static final String[] TIME_ENTRY_TYPES_C_UPDATED_AT = { "updated_at", INTEGER };
+	// public static final String[][] TIME_ENTRY_TYPES_COLUMNS = { TIME_ENTRY_TYPES_C_FIRST_NAME, TIME_ENTRY_TYPES_C_UPDATED_AT };
+
+	// Address table
+	// public static final String ADDRESSES_TABLE_NAME = "addresses";
+	// private static final String ID_AND_SERVER_ID = BaseColumns._ID + " INTEGER PRIMARY KEY ASC, server_id INTEGER, ";
+	// public static final String CREATE_ADRESSES = "CREATE TABLE " + TABLE_ADDRESSES + " (" + ID_AND_SERVER_ID + "line1 TEXT, line2 TEXT, line3 TEXT, zip INTEGER, place TEXT);";
+
+	private static final String DATABASE_NAME = "mrt.db";
 	private static final int DATABASE_VERSION = 1;
 
-	public static final String TABLE_ADDRESSES = "addresses";
-	public static final String TABLE_TIME_ENTRIES = "time_entries";
-	public static final String TABLE_TIME_ENTRY_TYPES = "time_entry_types";
-	public static final String TABLE_CUSTOMERS = "customers";
+	public static final String TEXT = "TEXT";
+	public static final String INTEGER = "INTEGER";
+	public static final String BLOB = "BLOB";
 
-	private static final String ID_AND_SERVER_ID = BaseColumns._ID + " INTEGER PRIMARY KEY ASC, server_id INTEGER, ";
-	public static final String CREATE_ADRESSES = "CREATE TABLE " + TABLE_ADDRESSES + " (" + ID_AND_SERVER_ID + "line1 TEXT, line2 TEXT, line3 TEXT, zip INTEGER, place TEXT);";
-	public static final String CREATE_TIME_ENTRIES = "CREATE TABLE " + TABLE_TIME_ENTRIES + " (" + ID_AND_SERVER_ID
-			+ " hashcode TEXT, time_start INTEGER, time_stop INTEGER, description TEXT, position TEXT, audo_record BLOB, customer_id INTEGER, time_entry_type_id INTEGER);";
-	public static final String CREATE_TIME_ENTRY_TYPES = "CREATE TABLE " + TABLE_TIME_ENTRY_TYPES + " (" + ID_AND_SERVER_ID + " description TEXT, updated_at INTEGER);";
-	public static final String CREATE_TIME_CUSTOMERS = "CREATE TABLE " + TABLE_CUSTOMERS + " (" + ID_AND_SERVER_ID
-			+ " first_name TEXT, last_name TEXT, phone TEXT, position TEXT, updated_at INTEGER, address_id INTEGER);";
+	public static final String TIME_ENTRIES_TABLE_NAME = "time_entry_types";
+	public static final String[] TIME_ENTRIES_C_HASHCODE = { "hashcode", TEXT };
+	public static final String[] TIME_ENTRIES_C_DESCRIPTION = { "description", TEXT };
+	public static final String[] TIME_ENTRIES_C_POSITION = { "position", TEXT };
+	public static final String[] TIME_ENTRIES_C_AUDIO_RECORD = { "audo_record", BLOB };
+	public static final String[] TIME_ENTRIES_C_TIME_START = { "time_start", INTEGER };
+	public static final String[] TIME_ENTRIES_C_TIME_STOP = { "time_stop", INTEGER };
+	public static final String[] TIME_ENTRIES_C_CUSTOMER_ID = { "customer_id", INTEGER };
+	public static final String[] TIME_ENTRIES_C_TIME_ENTRY_TYPE_ID = { "time_entry_type_id", INTEGER };
+	public static final String[][] TIME_ENTRIES_COLUMNS = { TIME_ENTRIES_C_HASHCODE, TIME_ENTRIES_C_DESCRIPTION, TIME_ENTRIES_C_POSITION, TIME_ENTRIES_C_AUDIO_RECORD, TIME_ENTRIES_C_TIME_START,
+			TIME_ENTRIES_C_TIME_STOP, TIME_ENTRIES_C_CUSTOMER_ID, TIME_ENTRIES_C_TIME_ENTRY_TYPE_ID };
 
-	private static final String TABLES_CREATE = CREATE_ADRESSES + CREATE_TIME_CUSTOMERS + CREATE_TIME_ENTRIES + CREATE_TIME_ENTRY_TYPES;
-	private static final String TABLES_DELETE = "DROP TABLE " + TABLE_ADDRESSES + ";" + "DROP TABLE " + TABLE_TIME_ENTRIES + ";" + "DROP TABLE " + TABLE_TIME_ENTRY_TYPES + ";" + "DROP TABLE "
-			+ TABLE_CUSTOMERS + ";";
+	/* Add the other tables */
+	private static final String[][][][] TABLE_DEFINITIONS = new String[][][][] { { { { TIME_ENTRIES_TABLE_NAME } }, TIME_ENTRIES_COLUMNS } };
+
+	private Context context;
+	private SQLiteDatabase db;
 
 	public DbHelper(Context context) {
-		super(context, Config.DATABASE_NAME, null, DATABASE_VERSION); // context, name, factory, version
+		this.context = context;
+		OpenHelper openHelper = new OpenHelper(this.context);
+		this.db = openHelper.getWritableDatabase();
 	}
 
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-		db.execSQL(TABLES_CREATE);
+	private static String generateCreateTablesScript() {
+		String generatedScript = "";
+		for (String[][][] tableDefinition : TABLE_DEFINITIONS) {
+			String tableName = tableDefinition[0][0][0];
+			String[][] tableColumns = tableDefinition[1];
+			generatedScript += generateCreateTablesScript(tableName, tableColumns) + "\n";
+		}
+		return generatedScript;
 	}
 
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		db.execSQL(TABLES_DELETE);
-		db.execSQL(TABLES_CREATE);
+	private static String generateCreateTablesScript(String tableName, String[][] tableColumns) {
+		String generatedScript = "CREATE TABLE " + tableName + " (";
+		boolean first = false;
+		for (String[] tableColumn : tableColumns) {
+			if (first) {
+				first = false;
+			} else {
+				generatedScript += ", ";
+			}
+			String tableColumnName = tableColumn[0], tableColumnType = tableColumn[1];
+			generatedScript += tableColumnName + " " + tableColumnType;
+		}
+		generatedScript += ");";
+		return generatedScript;
 	}
 
-	public static SQLiteDatabase getWriteableDb(Context applicationContext) {
-		return new DbHelper(applicationContext).getWritableDatabase();
+	private static String generateDropTablesScript() {
+		String generatedScript = "";
+		for (String[][][] tableDefinition : TABLE_DEFINITIONS) {
+			String tableName = tableDefinition[0][0][0];
+			generatedScript += generateDropTablesScript(tableName) + "\n";
+		}
+		return generatedScript;
 	}
-	public static SQLiteDatabase getReadableDb(Context applicationContext) {
-		return new DbHelper(applicationContext).getReadableDatabase();
+
+	private static String generateDropTablesScript(String tableName) {
+		return "DROP TABLE IF EXISTS " + tableName + ";";
+	}
+
+	private static class OpenHelper extends SQLiteOpenHelper {
+
+		OpenHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL(generateCreateTablesScript());
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			db.execSQL(generateDropTablesScript());
+			onCreate(db);
+		}
 	}
 }
