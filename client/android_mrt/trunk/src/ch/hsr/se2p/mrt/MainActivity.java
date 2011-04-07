0da0593014a88b39bc67a3b55cf990f6f31b1c48
Lukas Elmer <lukas.elmer@gmail.com>
2011-04-07 13:50:55 +0000

    refs #275 * Replace old data mapping with ORMLite

--- client/android_mrt/trunk/src/ch/hsr/se2p/mrt/MainActivity.java
+++ client/android_mrt/trunk/src/ch/hsr/se2p/mrt/MainActivity.java
@@ -1,19 +1,34 @@
 package ch.hsr.se2p.mrt;
 
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
-import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import ch.hsr.se2p.mrt.models.DbHelper;
+import ch.hsr.se2p.mrt.models.DatabaseHelper;
 import ch.hsr.se2p.mrt.models.TimeEntry;
 import ch.hsr.se2p.mrt.network.Transmitter;
 
-public class MainActivity extends Activity {
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.android.apptools.OpenHelperManager.SqliteOpenHelperFactory;
+import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+
+public class MainActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 
 	public static final String TAG = MainActivity.class.getSimpleName();
-	private DbHelper dbh;
+
+	static {
+		OpenHelperManager.setOpenHelperFactory(new SqliteOpenHelperFactory() {
+			@Override
+			public OrmLiteSqliteOpenHelper getHelper(Context context) {
+				return new DatabaseHelper(context);
+			}
+		});
+	}
 
 	/** Called when the activity is first created. */
 	@Override
@@ -21,78 +36,94 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		try {
-			dbh = new DbHelper(getApplicationContext());
+		createSomeTimeEntries();
+		transmitTheTimeEnties();
 
-			createSomeTimeEntries();
-			transmitTheTimeEnties();
-		} finally {
-			dbh.close();
-		}
-
-		// TimeEntry.Values values = new TimeEntry.Values();
-		// values.description = "bla";
-		//
-		// long id = TimeEntry.create(db, values);
-		// Log.d(TAG, "TimeEntry with ID " + id + " created");
-		//
-		// c = TimeEntry.getById(db, id);
-		// Log.d(TAG, "c.getCount(): " + c.getCount());
-		// Log.d(TAG, "c.c.getString(0): " + c.getString(0));
-		// Log.d(TAG, "c.c.getString(1): " + c.getString(1));
-		// Log.d(TAG, "c.c.getString(2): " + c.getString(2));
-		// Log.d(TAG, "c.c.getString(3): " + c.getString(3));
-		// Log.d(TAG, "c.c.getString(4): " + c.getString(4));
-
-		// final Button button = (Button) findViewById(R.id.login_button);
-		// final EditText usernameEditText = (EditText)
-		// findViewById(R.id.login), passwordEditText = (EditText)
-		// findViewById(R.id.password);
-		// String[] usernameAndPassword = loadLoginFromCache();
-		// if (usernameAndPassword != null) {
-		// doRequests(usernameAndPassword[0], usernameAndPassword[1]);
-		// }
-		// button.setOnClickListener(new View.OnClickListener() {
-		// public void onClick(View v) {
-		// String username = usernameEditText.getText().toString(), password =
-		// passwordEditText.getText().toString();
-		// doRequests(username, password);
-		// }
-		// });
 	}
 
 	private void createSomeTimeEntries() {
-		TimeEntry t1 = new TimeEntry(new Timestamp(System.currentTimeMillis()));
-		t1.setDescription("with description, time is " + System.currentTimeMillis());
-		t1.setTimeStop(new Timestamp(System.currentTimeMillis() + (1000 * 60 * 60 * 4))); // 4h later
-		long id1 = t1.create(dbh);
-		Log.i(TAG, "Inserted ID: " + id1);
+		try {
+			Dao<TimeEntry, Integer> timeEntryDao = getHelper().getTimeEntryDao();
 
-		TimeEntry t2 = new TimeEntry(new Timestamp(System.currentTimeMillis()));
-		t2.setTimeStop(new Timestamp(System.currentTimeMillis() + (1000 * 60 * 60 * 3))); // 3h later
-		long id2 = t2.create(dbh);
-		Log.i(TAG, "Inserted ID: " + id2);
+			TimeEntry t1 = new TimeEntry(new Timestamp(System.currentTimeMillis()));
+			t1.setDescription("with description, time is " + System.currentTimeMillis());
+			t1.setTimeStop(new Timestamp(System.currentTimeMillis() + (1000 * 60 * 60 * 4))); // 4h later
+			int id1 = timeEntryDao.create(t1);
+			Log.i(TAG, "Inserted ID: " + id1);
 
-		TimeEntry t3 = new TimeEntry(new Timestamp(System.currentTimeMillis()));
-		t3.setTimeStop(new Timestamp(System.currentTimeMillis() + (1000 * 60 * 30))); // 30min later
-		long id3 = t3.create(dbh);
-		Log.i(TAG, "Inserted ID: " + id3);
+			TimeEntry t2 = new TimeEntry(new Timestamp(System.currentTimeMillis()));
+			t2.setTimeStop(new Timestamp(System.currentTimeMillis() + (1000 * 60 * 60 * 3))); // 3h later
+			int id2 = timeEntryDao.create(t1);
+			Log.i(TAG, "Inserted ID: " + id2);
+
+			TimeEntry t3 = new TimeEntry(new Timestamp(System.currentTimeMillis()));
+			t3.setTimeStop(new Timestamp(System.currentTimeMillis() + (1000 * 60 * 30))); // 30min later
+			int id3 = timeEntryDao.create(t1);
+			Log.i(TAG, "Inserted ID: " + id3);
+		} catch (SQLException e) {
+			Log.e(TAG, "Database excaeption", e);
+		}
 	}
 
 	private void transmitTheTimeEnties() {
-		List<TimeEntry> l = TimeEntry.all(dbh);
-		Log.d(TAG, "Size: " + l.size());
-		Transmitter transmitter = new Transmitter();
-		for (TimeEntry timeEntry : l) {
-			if (transmitter.transmit(timeEntry)) {
-				if (!timeEntry.isTransmitted()) {
-					timeEntry.setTransmitted(dbh);
-				}
-				if (transmitter.confirm(timeEntry)) {
-					timeEntry.delete(dbh);
+		try {
+			Dao<TimeEntry, Integer> timeEntryDao = getHelper().getTimeEntryDao();
+
+			List<TimeEntry> l = timeEntryDao.queryForAll();
+			Log.d(TAG, "Size: " + l.size());
+			Transmitter transmitter = new Transmitter();
+			for (TimeEntry timeEntry : l) {
+				if (transmitter.transmit(timeEntry)) {
+					if (!timeEntry.isTransmitted()) {
+						timeEntry.setTransmitted();
+						timeEntryDao.update(timeEntry);
+					}
+					if (transmitter.confirm(timeEntry)) {
+						timeEntryDao.delete(timeEntry);
+					}
 				}
 			}
+		} catch (SQLException e) {
+			Log.e(TAG, "Database excaeption", e);
 		}
 	}
 
 }
+
+//
+// try {
+// dbh = new DbHelper(getApplicationContext());
+//
+// } finally {
+// dbh.close();
+// }
+
+// TimeEntry.Values values = new TimeEntry.Values();
+// values.description = "bla";
+//
+// long id = TimeEntry.create(db, values);
+// Log.d(TAG, "TimeEntry with ID " + id + " created");
+//
+// c = TimeEntry.getById(db, id);
+// Log.d(TAG, "c.getCount(): " + c.getCount());
+// Log.d(TAG, "c.c.getString(0): " + c.getString(0));
+// Log.d(TAG, "c.c.getString(1): " + c.getString(1));
+// Log.d(TAG, "c.c.getString(2): " + c.getString(2));
+// Log.d(TAG, "c.c.getString(3): " + c.getString(3));
+// Log.d(TAG, "c.c.getString(4): " + c.getString(4));
+
+// final Button button = (Button) findViewById(R.id.login_button);
+// final EditText usernameEditText = (EditText)
+// findViewById(R.id.login), passwordEditText = (EditText)
+// findViewById(R.id.password);
+// String[] usernameAndPassword = loadLoginFromCache();
+// if (usernameAndPassword != null) {
+// doRequests(usernameAndPassword[0], usernameAndPassword[1]);
+// }
+// button.setOnClickListener(new View.OnClickListener() {
+// public void onClick(View v) {
+// String username = usernameEditText.getText().toString(), password =
+// passwordEditText.getText().toString();
+// doRequests(username, password);
+// }
+// });
--- client/android_mrt/trunk/src/ch/hsr/se2p/mrt/models/TimeEntry.java
+++ client/android_mrt/trunk/src/ch/hsr/se2p/mrt/models/TimeEntry.java
@@ -4,30 +4,31 @@ import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.sql.Blob;
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONStringer;
 
-import ch.hsr.se2p.mrt.network.Transmitter;
-
-import android.content.ContentValues;
-import android.database.Cursor;
 import android.location.Location;
-import android.os.Parcel;
 import android.util.Log;
+
+import com.j256.ormlite.field.DatabaseField;
 
 public class TimeEntry implements JSONObjectable {
 	private static final String TAG = TimeEntry.class.getSimpleName();
 
-	private static String TABLE_NAME = DbHelper.TIME_ENTRIES_TABLE_NAME;
-
-	private Integer id, railsId, customerId, timeEntryTypeId, transmitted;
+	@DatabaseField(generatedId = true)
+	private Integer id;
+	@DatabaseField
+	private Integer railsId, customerId, timeEntryTypeId;
+	@DatabaseField
+	private boolean transmitted;
+	@DatabaseField
 	private String hashcode, description;
+	@DatabaseField
 	private Timestamp timeStart, timeStop;
+	@DatabaseField
 	private Location position;
+	@DatabaseField
 	private Blob audoRecord;
 
 	private static SecureRandom random = new SecureRandom();
@@ -37,66 +38,8 @@ public class TimeEntry implements JSONObjectable {
 		this.timeStart = timeStart;
 	}
 
-	public TimeEntry(Cursor c) {
-		id = c.getInt(c.getColumnIndex(DbHelper.C_ID[0]));
-		customerId = c.getInt(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_CUSTOMER_ID[0]));
-		timeEntryTypeId = c.getInt(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_TIME_ENTRY_TYPE_ID[0]));
-		hashcode = c.getString(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_HASHCODE[0]));
-		description = c.getString(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_DESCRIPTION[0]));
-		timeStart = Timestamp.valueOf(c.getString(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_TIME_START[0])));
-		timeStop = Timestamp.valueOf(c.getString(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_TIME_STOP[0])));
-		transmitted = c.getInt(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_TRANSMITTED[0]));
-		initPosition(c);
-		initAudioRecord(c);
-	}
-
-	private void initAudioRecord(Cursor c) {
-		// TODO: Implement this, DbHelper.TIME_ENTRIES_C_AUDIO_RECORD[0]
-	}
-
-	protected void initPosition(Cursor c) {
-		String s = c.getString(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_POSITION[0]));
-		if (s.length() > 0) {
-			Parcel p = Parcel.obtain();
-			p.writeString(s);
-			Location.CREATOR.createFromParcel(p);
-		}
-	}
-
-	public long create(DbHelper dbh) {
-		if (timeStart == null || timeStop == null) {
-			return -1;
-		}
-		return dbh.insert(TABLE_NAME, generateContentValues());
-	}
-
-	public void setTransmitted(DbHelper dbh) {
-		transmitted = new Integer(1);
-		dbh.update(TABLE_NAME, generateContentValues(), id);
-	}
-
-	protected ContentValues generateContentValues() {
-		ContentValues contentValues = new ContentValues();
-		contentValues.put(DbHelper.TIME_ENTRIES_C_CUSTOMER_ID[0], customerId == null ? "" : customerId.toString());
-		contentValues.put(DbHelper.TIME_ENTRIES_C_TIME_ENTRY_TYPE_ID[0], timeEntryTypeId == null ? "" : timeEntryTypeId.toString());
-		contentValues.put(DbHelper.TIME_ENTRIES_C_HASHCODE[0], hashcode);
-		contentValues.put(DbHelper.TIME_ENTRIES_C_DESCRIPTION[0], description);
-		contentValues.put(DbHelper.TIME_ENTRIES_C_TIME_START[0], timeStart.toString());
-		contentValues.put(DbHelper.TIME_ENTRIES_C_TIME_STOP[0], timeStop.toString());
-		contentValues.put(DbHelper.TIME_ENTRIES_C_TRANSMITTED[0], transmitted.toString());
-		contentValues.put(DbHelper.TIME_ENTRIES_C_AUDIO_RECORD[0], audoRecord == null ? "" : audoRecord.toString());
-		contentValues.put(DbHelper.TIME_ENTRIES_C_POSITION[0], getPositionString());
-		return contentValues;
-	}
-
-	protected String getPositionString() {
-		String postitionString = "";
-		if (position != null) {
-			Parcel p = Parcel.obtain();
-			position.writeToParcel(p, 0);
-			postitionString = p.readString();
-		}
-		return postitionString;
+	public void setTransmitted() {
+		transmitted = true;
 	}
 
 	@Override
@@ -115,24 +58,6 @@ public class TimeEntry implements JSONObjectable {
 			Log.e(TAG, "Error creating JSON Object", e);
 		}
 		return j;
-	}
-
-	public void delete(DbHelper dbh) {
-		dbh.delete(TABLE_NAME, id);
-	}
-
-	public static List<TimeEntry> all(DbHelper dbh) {
-		ArrayList<TimeEntry> l = new ArrayList<TimeEntry>();
-		Cursor c = dbh.all(TABLE_NAME);
-		while (c.moveToNext()) {
-			l.add(new TimeEntry(c));
-		}
-		c.close();
-		return l;
-	}
-
-	public static Cursor getById(DbHelper dbh, long id) {
-		return dbh.findById(TABLE_NAME, id);
 	}
 
 	public Integer getId() {
@@ -200,6 +125,55 @@ public class TimeEntry implements JSONObjectable {
 	}
 
 	public boolean isTransmitted() {
-		return transmitted == 1;
+		return transmitted;
 	}
+
+	// public TimeEntry(Cursor c) {
+	// id = c.getInt(c.getColumnIndex(DbHelper.C_ID[0]));
+	// customerId = c.getInt(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_CUSTOMER_ID[0]));
+	// timeEntryTypeId = c.getInt(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_TIME_ENTRY_TYPE_ID[0]));
+	// hashcode = c.getString(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_HASHCODE[0]));
+	// description = c.getString(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_DESCRIPTION[0]));
+	// timeStart = Timestamp.valueOf(c.getString(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_TIME_START[0])));
+	// timeStop = Timestamp.valueOf(c.getString(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_TIME_STOP[0])));
+	// transmitted = c.getInt(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_TRANSMITTED[0]));
+	// initPosition(c);
+	// initAudioRecord(c);
+	// }
+	//
+	// private void initAudioRecord(Cursor c) {
+	// // TODO: Implement this, DbHelper.TIME_ENTRIES_C_AUDIO_RECORD[0]
+	// }
+
+	// protected void initPosition(Cursor c) {
+	// String s = c.getString(c.getColumnIndex(DbHelper.TIME_ENTRIES_C_POSITION[0]));
+	// if (s.length() > 0) {
+	// Parcel p = Parcel.obtain();
+	// p.writeString(s);
+	// Location.CREATOR.createFromParcel(p);
+	// }
+	// }
+	// protected ContentValues generateContentValues() {
+	// ContentValues contentValues = new ContentValues();
+	// contentValues.put(DbHelper.TIME_ENTRIES_C_CUSTOMER_ID[0], customerId == null ? "" : customerId.toString());
+	// contentValues.put(DbHelper.TIME_ENTRIES_C_TIME_ENTRY_TYPE_ID[0], timeEntryTypeId == null ? "" : timeEntryTypeId.toString());
+	// contentValues.put(DbHelper.TIME_ENTRIES_C_HASHCODE[0], hashcode);
+	// contentValues.put(DbHelper.TIME_ENTRIES_C_DESCRIPTION[0], description);
+	// contentValues.put(DbHelper.TIME_ENTRIES_C_TIME_START[0], timeStart.toString());
+	// contentValues.put(DbHelper.TIME_ENTRIES_C_TIME_STOP[0], timeStop.toString());
+	// contentValues.put(DbHelper.TIME_ENTRIES_C_TRANSMITTED[0], transmitted.toString());
+	// contentValues.put(DbHelper.TIME_ENTRIES_C_AUDIO_RECORD[0], audoRecord == null ? "" : audoRecord.toString());
+	// contentValues.put(DbHelper.TIME_ENTRIES_C_POSITION[0], getPositionString());
+	// return contentValues;
+	// }
+	// protected String getPositionString() {
+	// String postitionString = "";
+	// if (position != null) {
+	// Parcel p = Parcel.obtain();
+	// position.writeToParcel(p, 0);
+	// postitionString = p.readString();
+	// }
+	// return postitionString;
+	// }
+
 }
