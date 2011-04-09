27335563e0a7fcf1c49c69486458a18906c376b8
Lukas Elmer <lukas.elmer@gmail.com>
2011-04-09 17:06:54 +0000

    refs #275

---
+++ client/android_mrt/trunk/test/ch/hsr/se2p/mrt/test/MainActivityTest.java
@@ -0,0 +1,35 @@
+package ch.hsr.se2p.mrt.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.TextView;
+import ch.hsr.se2p.mrt.MainActivity;
+import ch.hsr.se2p.mrt.R;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+	private MainActivity mActivity;
+	private TextView mView;
+	private String resourceString;
+
+	public MainActivityTest() {
+		super(MainActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		mActivity = this.getActivity();
+		mView = (TextView) mActivity.findViewById(R.id.textview);
+		resourceString = mActivity.getString(R.string.hello);
+	}
+
+	public void testPreconditions() {
+		assertNotNull(mView);
+	}
+
+	public void testText() {
+		assertEquals(resourceString, (String) mView.getText());
+		// assertEquals(resourceString, (String) mView.getText());
+	}
+}
