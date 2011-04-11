191425858020e06e7fa8d6354c11145940d03686
Lukas Elmer <lukas.elmer@gmail.com>
2011-04-11 16:30:18 +0000

    refs #276 * Style it

--- server/trunk/app/controllers/time_entries_controller.rb
+++ server/trunk/app/controllers/time_entries_controller.rb
@@ -34,19 +34,36 @@ class TimeEntriesController < ApplicationController
 
   # POST /time_entries
   def create
-    @time_entry = TimeEntry.find_by_hashcode(params[:time_entry][:hashcode]) || TimeEntry.new(params[:time_entry])
-
     respond_to do |format|
-      if (!@time_entry.new_record?) || ((!params[:time_entry][:hashcode].blank?) && @time_entry.save)
-        format.html { redirect_to(@time_entry, :notice => 'Time entry was successfully created.') }
-        format.json { render :json => @time_entry, :status => :created, :location => @time_entry  }
-      else
-        format.html { render :action => "new" }
-        format.json  { render :json => @time_entry.errors, :status => :unprocessable_entity }
-      end
+      format.html { create_by_html }
+      format.json { create_by_json }
     end
   end
 
+  private
+  def create_by_html
+    @time_entry = TimeEntry.new(params[:time_entry])
+    @time_entry.hashcode = nil
+    if @time_entry.save
+      redirect_to(@time_entry, :notice => 'Time entry was successfully created.')
+    else
+      render :action => "new"
+    end
+  end
+
+  def create_by_json
+    # Case 1: Hashcode is blank and error is rendered
+    render :json => "Hashcode blank", :status => :unprocessable_entity and return if params[:time_entry][:hashcode].blank?
+    # Case 2: TimeEntry exists and is rendered
+    @time_entry = TimeEntry.find_by_hashcode(params[:time_entry][:hashcode])
+    render :json => @time_entry, :status => :created, :location => @time_entry and return if !@time_entry.nil?
+    # Case 3: TimeEntry does not exists and is created
+    @time_entry = TimeEntry.new(params[:time_entry])
+    render :json => @time_entry.errors, :status => :unprocessable_entity and return if !@time_entry.save
+    render :json => @time_entry, :status => :created, :location => @time_entry
+  end
+
+  public
   # PUT /time_entries/1
   def update
     @time_entry = TimeEntry.find(params[:id])
