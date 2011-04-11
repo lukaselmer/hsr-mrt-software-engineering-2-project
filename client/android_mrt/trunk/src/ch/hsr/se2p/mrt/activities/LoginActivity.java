94cad00c54c1bf53073efa0a65032242934d512c
Lukas Elmer <lukas.elmer@gmail.com>
2011-04-11 17:28:22 +0000

    refs #275 Add login activity

---
+++ client/android_mrt/trunk/src/ch/hsr/se2p/mrt/activities/LoginActivity.java
@@ -0,0 +1,20 @@
+package ch.hsr.se2p.mrt.activities;
+
+import android.content.Context;
+import ch.hsr.se2p.mrt.persistence.database.DatabaseHelper;
+
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.android.apptools.OpenHelperManager.SqliteOpenHelperFactory;
+
+public class LoginActivity extends OrmLiteBaseActivity<DatabaseHelper> {
+	static {
+		OpenHelperManager.setOpenHelperFactory(new SqliteOpenHelperFactory() {
+			@Override
+			public OrmLiteSqliteOpenHelper getHelper(Context context) {
+				return new DatabaseHelper(context);
+			}
+		});
+	}
+}
