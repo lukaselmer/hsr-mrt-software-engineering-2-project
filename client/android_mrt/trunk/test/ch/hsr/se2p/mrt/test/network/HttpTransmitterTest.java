3cb631b68afe1b780990cdf77edba585a0a3afda
Lukas Elmer <lukas.elmer@gmail.com>
2011-04-12 07:35:49 +0000

    refs #275 * Add some more tests

--- client/android_mrt/trunk/test/ch/hsr/se2p/mrt/test/network/HttpTransmitterTest.java
+++ client/android_mrt/trunk/test/ch/hsr/se2p/mrt/test/network/HttpTransmitterTest.java
@@ -1,5 +1,20 @@
 package ch.hsr.se2p.mrt.test.network;
 
-public class HttpTransmitterTest {
+import ch.hsr.se2p.mrt.network.HttpTransmitter;
+import android.test.AndroidTestCase;
+import android.test.mock.MockContext;
 
+public class HttpTransmitterTest extends AndroidTestCase {
+	private HttpTransmitter transmitter;
+
+	@Override
+	protected void setUp() throws Exception {
+		MockContext c = new MockContext();
+		setContext(c);
+		transmitter = new HttpTransmitter();
+	}
+
+	public void testBla() {
+		assertTrue(true);
+	}
 }
