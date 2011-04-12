909c0919974c1838590ad4626c51755a96a71452
Lukas Elmer <lukas.elmer@gmail.com>
2011-04-12 15:30:26 +0000

    refs #275

--- client/android_mrt/trunk/test/ch/hsr/se2p/mrt/persistence/TimeEntryTest.java
+++ client/android_mrt/trunk/test/ch/hsr/se2p/mrt/persistence/TimeEntryTest.java
@@ -1,4 +1,4 @@
-package ch.hsr.se2p.mrt.test.persistence;
+package ch.hsr.se2p.mrt.persistence;
 
 import java.sql.SQLException;
 import java.sql.Timestamp;
--- client/android_mrt/trunk/test/ch/hsr/se2p/mrt/services/SynchronizationServiceTest.java
+++ client/android_mrt/trunk/test/ch/hsr/se2p/mrt/services/SynchronizationServiceTest.java
@@ -1,4 +1,4 @@
-package ch.hsr.se2p.mrt.test.services;
+package ch.hsr.se2p.mrt.services;
 
 public class SynchronizationServiceTest {
 	// TODO: implement this
