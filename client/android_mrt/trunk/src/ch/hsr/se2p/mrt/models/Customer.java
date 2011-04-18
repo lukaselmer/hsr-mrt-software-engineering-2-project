c518485357c187f5f771e0034950ba4fe79400db
Christina Heidt <christina.heidt@elmer.ch>
2011-04-18 12:58:32 +0000

    refs #148

--- client/android_mrt/trunk/src/ch/hsr/se2p/mrt/models/Customer.java
+++ client/android_mrt/trunk/src/ch/hsr/se2p/mrt/models/Customer.java
@@ -3,27 +3,20 @@ package ch.hsr.se2p.mrt.models;
 import android.provider.BaseColumns;
 
 public class Customer implements BaseColumns {
-	// private static String TABLE_NAME = DbHelper.TABLE_CUSTOMERS;
-	//
-	// public class Values {
-	// public String firstName, lastName, phone;
-	// public Location position;
-	// public Timestamp updatedAt;
-	// }
-	//
-	// private Values values = new Values();
-	//
-	// public static long create(SQLiteDatabase db, Values values) {
-	// ContentValues initialValues = new ContentValues();
-	// initialValues.put("first_name", values.firstName);
-	// initialValues.put("last_name", values.lastName);
-	// initialValues.put("phone", values.phone);
-	// initialValues.put("position", values.position.toString());
-	// initialValues.put("updated_at", values.updatedAt.toString());
-	// return db.insert(TABLE_NAME, null, initialValues);
-	// }
-	//
-	// public static void delete(SQLiteDatabase db, long id) {
-	// db.delete(TABLE_NAME, BaseColumns._ID + "id=" + id, null);
-	// }
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	
+	
+	public Customer(Integer id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+
+	public String toString(){
+		return lastName + " " + firstName;
+	}
 }
