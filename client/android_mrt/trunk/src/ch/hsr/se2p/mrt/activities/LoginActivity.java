deaa1b2dfc457a7c9a0b3089738a9415b787063b
Delia Treichler <delia.teichler@elmer.ch>
2011-04-18 20:25:08 +0000

    refs #306
    Layout

--- client/android_mrt/trunk/src/ch/hsr/se2p/mrt/activities/LoginActivity.java
+++ client/android_mrt/trunk/src/ch/hsr/se2p/mrt/activities/LoginActivity.java
@@ -1,6 +1,15 @@
 package ch.hsr.se2p.mrt.activities;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import ch.hsr.se2p.mrt.R;
 import ch.hsr.se2p.mrt.database.DatabaseHelper;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
@@ -17,4 +26,44 @@ public class LoginActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 			}
 		});
 	}
+
+	private EditText editUsername;
+	private EditText editPassword;
+
+	/** Called when the activity is first created. */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings);
+
+		Button okBtn = (Button) findViewById(R.id.loginButton);
+		okBtn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				int usernameSize = editUsername.getText().length();
+				int passwordSize = editPassword.getText().length();
+				if (usernameSize == 0 || passwordSize == 0) {
+					displayAlertDialog();
+				} else {
+					ProgressDialog.show(LoginActivity.this, "", "Ladevorgang. Bitte warten...", true);
+					// doLogin(editUsername.getText().toString(), editPassword.getText().toString());
+				}
+			}
+
+		});
+	}
+
+	protected void displayAlertDialog() {
+		AlertDialog.Builder dialogAlerting = new AlertDialog.Builder(LoginActivity.this);
+		dialogAlerting.setMessage("Bitte Benutzername und Passwort angeben!");
+		dialogAlerting.setTitle("Fehler");
+		dialogAlerting.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int id) {
+				dialog.cancel();
+			}
+		});
+		dialogAlerting.create();
+	}
+
 }
