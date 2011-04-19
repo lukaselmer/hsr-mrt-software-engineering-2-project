901857aef43f00f6bdb60646fd6c0ba185856196
Delia Treichler <delia.teichler@elmer.ch>
2011-04-19 21:24:05 +0000

    refs #306
    Tests

--- client/android_mrt/trunk/test/ch/hsr/se2p/mrt/activities/LoginActivityTest.java
+++ client/android_mrt/trunk/test/ch/hsr/se2p/mrt/activities/LoginActivityTest.java
@@ -1,7 +1,61 @@
 package ch.hsr.se2p.mrt.activities;
 
-import android.app.Activity;
+import ch.hsr.se2p.mrt.R;
+import android.app.AlertDialog;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
-public class LoginActivityTest extends Activity {
+public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity>{
+	public LoginActivityTest(String pkg, Class<LoginActivity> activityClass) {
+		super("ch.hsr.se2p.mrt.activities", LoginActivity.class);
+		// TODO Auto-generated constructor stub
+	}
+	
+	private LoginActivity activity;
+    private EditText editUsername;
+    private EditText editPasswort;
+    private CheckBox checkbox;
+	
+    @Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		activity = this.getActivity();
+        editUsername = (EditText) activity.findViewById(R.id.editUsername);
+        editPasswort = (EditText) activity.findViewById(R.id.editPassword);
+        checkbox = (CheckBox) activity.findViewById(R.id.cBsaveLogin);
+	}
+	
+	public void testPreconditions() {
+	    assertNull(editUsername);
+	    assertNull(editPasswort);
+	    assertEquals(true, checkbox.isChecked());
+	}
+	
+	public void testNoLoginDataSupplied() {
+		editUsername = null;
+		editPasswort = null;
+		//AlertDialog.Builder b = new AlertDialog.Builder(ch.hsr.se2p.mrt.activities.LoginActivity);
+		//b.setTitle("Fehler");
+		//b.setMessage("Bitte Benutzernamen und Passwort angeben!");
+				
+		assertNull(editUsername);
+		assertNull(editPasswort);
+		//assertTrue(b.isShowing());
+		
+	}
+	
+	public void testNoUsernameSupplied() {
+		
+	}
+	public void testNoPasswordSupplied() {
+		
+	}
 
+	
+//	@Override
+//	protected void tearDown() throws Exception {
+//		// TODO Auto-generated method stub
+//		super.tearDown();
+//	}
 }
