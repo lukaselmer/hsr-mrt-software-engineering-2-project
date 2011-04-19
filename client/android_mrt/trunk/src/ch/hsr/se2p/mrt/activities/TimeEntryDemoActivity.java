6b621a862bc87dad120fd06d4d0b854ba12e46b2
Christina Heidt <christina.heidt@elmer.ch>
2011-04-19 12:03:30 +0000

    refs #148

--- client/android_mrt/trunk/src/ch/hsr/se2p/mrt/activities/TimeEntryDemoActivity.java
+++ client/android_mrt/trunk/src/ch/hsr/se2p/mrt/activities/TimeEntryDemoActivity.java
@@ -2,8 +2,12 @@ package ch.hsr.se2p.mrt.activities;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import ch.hsr.se2p.mrt.R;
 import ch.hsr.se2p.mrt.database.DatabaseHelper;
+import ch.hsr.se2p.mrt.models.Customer;
+import ch.hsr.se2p.mrt.network.CustomerHelper;
 
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
@@ -19,9 +23,18 @@ public class TimeEntryDemoActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 			}
 		});
 	}
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.time_entry);
+
+		AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autocompleteCustomer);
+		ArrayAdapter<Customer> adapter = new ArrayAdapter<Customer>(this,
+				R.layout.list_item, CustomerHelper.hackForTest());
+		textView.setAdapter(adapter);
+
 	}
+
+	static final String[] customers = new String[] {"Anna", "Lena", "Babetta"};
 }
