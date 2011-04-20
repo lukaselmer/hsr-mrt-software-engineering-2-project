4921d3d4ad3422447f924081afe6ab8564ccd2a8
Christina Heidt <christina.heidt@elmer.ch>
2011-04-20 14:45:17 +0000

    refs #148

--- client/android_mrt/trunk/test/ch/hsr/se2p/mrt/activities/TimeEntryActivityTest.java
+++ client/android_mrt/trunk/test/ch/hsr/se2p/mrt/activities/TimeEntryActivityTest.java
@@ -1,5 +1,10 @@
 package ch.hsr.se2p.mrt.activities;
 
-public class TimeEntryActivityTest {
+import android.test.ActivityInstrumentationTestCase2;
 
+public class TimeEntryActivityTest extends ActivityInstrumentationTestCase2<TimeEntryActivity>  {
+
+	public TimeEntryActivityTest() {
+		super("ch.hsr.se2p.mrt", TimeEntryActivity.class);
+	}
 }
