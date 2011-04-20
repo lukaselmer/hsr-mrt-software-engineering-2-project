8964856eb666d46f04398cf5b456f7cd17301ce0
Delia Treichler <delia.teichler@elmer.ch>
2011-04-20 10:46:15 +0000

    refs #306

--- client/android_mrt/trunk/test/ch/hsr/se2p/mrt/activities/LoginActivityTest.java
+++ client/android_mrt/trunk/test/ch/hsr/se2p/mrt/activities/LoginActivityTest.java
@@ -1,12 +1,13 @@
 package ch.hsr.se2p.mrt.activities;
 
 import ch.hsr.se2p.mrt.R;
-import android.app.AlertDialog;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginActivity>{
+	
 	public LoginActivityTest(String pkg, Class<LoginActivity> activityClass) {
 		super("ch.hsr.se2p.mrt.activities", LoginActivity.class);
 		// TODO Auto-generated constructor stub
@@ -26,6 +27,12 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
         checkbox = (CheckBox) activity.findViewById(R.id.cBsaveLogin);
 	}
 	
+    @Override
+    public void runTestOnUiThread(Runnable r) throws Throwable {
+    	// TODO Auto-generated method stub
+    	super.runTestOnUiThread(r);
+    }
+    
 	public void testPreconditions() {
 	    assertNull(editUsername);
 	    assertNull(editPasswort);
@@ -38,12 +45,24 @@ public class LoginActivityTest extends ActivityInstrumentationTestCase2<LoginAct
 		//AlertDialog.Builder b = new AlertDialog.Builder(ch.hsr.se2p.mrt.activities.LoginActivity);
 		//b.setTitle("Fehler");
 		//b.setMessage("Bitte Benutzernamen und Passwort angeben!");
-				
+		
+		final Button loginBtn = (Button) activity.findViewById(R.id.loginButton);
+	    activity.runOnUiThread(new Runnable()
+	    {
+	        public void run()
+	        {
+	            loginBtn.performClick();
+	        }
+	    });
+	    
+	
+		
 		assertNull(editUsername);
 		assertNull(editPasswort);
 		//assertTrue(b.isShowing());
 		
 	}
+	
 	
 	public void testNoUsernameSupplied() {
 		
