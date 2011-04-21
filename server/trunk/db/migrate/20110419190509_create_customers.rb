0886a5b1c26f2b9e5bd819a54002b1bb3fb637b9
Remo Waltenspuel <remo.waltenspuel@elmermx.ch>
2011-04-21 13:01:09 +0000

    refs #294

--- server/trunk/db/migrate/20110419190509_create_customers.rb
+++ server/trunk/db/migrate/20110419190509_create_customers.rb
@@ -2,7 +2,6 @@ class CreateCustomers < ActiveRecord::Migration
   def self.up
     create_table :customers do |t|
       t.string :first_name
-      t.integer :id
       t.string :last_name
       t.string :phone
 
