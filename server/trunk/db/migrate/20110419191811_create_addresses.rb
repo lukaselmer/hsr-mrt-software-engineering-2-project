26363b0938bddc8af910cb9b54cf9a7e6a3673dd
Remo Waltenspuel <remo.waltenspuel@elmermx.ch>
2011-04-21 13:00:44 +0000

    refs #294

--- server/trunk/db/migrate/20110419191811_create_addresses.rb
+++ server/trunk/db/migrate/20110419191811_create_addresses.rb
@@ -1,7 +1,6 @@
 class CreateAddresses < ActiveRecord::Migration
   def self.up
     create_table :addresses do |t|
-      t.integer :id
       t.string :line1
       t.string :line2
       t.string :line3
