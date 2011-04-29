4c6c4b0030e18e9f9e572ecc2f5b64f55e31e72d
Christina Heidt <christina.heidt@elmer.ch>
2011-04-29 12:13:28 +0000

    refs #169 UserSettings Test

--- client/android_mrt/trunk/test/ch/hsr/se2p/mrt/models/UserSettingsTest.java
+++ client/android_mrt/trunk/test/ch/hsr/se2p/mrt/models/UserSettingsTest.java
@@ -1,5 +1,17 @@
 package ch.hsr.se2p.mrt.models;
 
-public class UserSettingsTest {
+import android.test.AndroidTestCase;
 
+public class UserSettingsTest extends AndroidTestCase {
+
+	private UserSettings settings;
+	private static final String WORKER_EMAIL = "field_worker@mrt.ch";
+	private static final String PASSWORD = "1234567890";
+	
+	public void testIntialUserSettings(){
+		settings = new UserSettings(1, WORKER_EMAIL, PASSWORD);
+		assertEquals(new Integer(1), settings.getUserId());
+		assertEquals(WORKER_EMAIL, settings.getEmail());
+		assertEquals(PASSWORD, settings.getPassword());
+	}
 }
