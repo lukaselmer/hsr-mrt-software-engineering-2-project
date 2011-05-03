eb655a49124ccb21f9100d856cdd9a40dc6155f4
Remo Waltenspuel <remo.waltenspuel@elmermx.ch>
2011-05-03 14:04:14 +0000

    refs #365

--- server/trunk/db/migrate/20110418124024_devise_create_users.rb
+++ server/trunk/db/migrate/20110418124024_devise_create_users.rb
@@ -7,6 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.integer :user_type
+      t.string :phone
 
       t.timestamps
     end
