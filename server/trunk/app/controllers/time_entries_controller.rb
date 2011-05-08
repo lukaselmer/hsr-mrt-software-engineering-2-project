fc07fef5ae6d5eb96646c068d4a9663ef1b0d1ca
Diego Steiner <diego.steiner@renuo.ch>
2011-05-08 14:47:17 +0000

    refs #309

--- server/trunk/app/controllers/time_entries_controller.rb
+++ server/trunk/app/controllers/time_entries_controller.rb
@@ -57,13 +57,19 @@ class TimeEntriesController < ApplicationController
   end
 
   def create_by_json
+    Rails.logger.debug params.inspect
+
     # Case 1: Hashcode is blank and error is rendered
     render :json => "Hashcode blank", :status => :unprocessable_entity and return if params[:time_entry][:hashcode].blank?
     # Case 2: TimeEntry exists and is rendered
     @time_entry = TimeEntry.find_by_hashcode(params[:time_entry][:hashcode])
     render :json => @time_entry, :status => :created, :location => @time_entry and return if !@time_entry.nil?
     # Case 3: TimeEntry does not exists and is created
+    @gps_position = GpsPosition.new(params[:time_entry][:gps_position_data])
+    params[:time_entry].delete :gps_position_data
+
     @time_entry = TimeEntry.new(params[:time_entry])
+    @time_entry.gps_position = @gps_position;
     @time_entry.user = current_user
     render :json => @time_entry.errors, :status => :unprocessable_entity and return if !@time_entry.save
     render :json => @time_entry, :status => :created, :location => @time_entry
