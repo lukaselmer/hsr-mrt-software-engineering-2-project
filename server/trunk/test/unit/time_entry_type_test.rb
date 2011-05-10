1572afaf188fcd75e06435cffb3050d7638b8b1c
Remo Waltenspuel <remo.waltenspuel@elmermx.ch>
2011-05-10 15:01:55 +0000

    refs #369

--- server/trunk/test/unit/time_entry_type_test.rb
+++ server/trunk/test/unit/time_entry_type_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class TimeEntryTypeTest < ActiveSupport::TestCase
 
   setup do
-    @time_entry_type_valid = time_entry_types :three
     @time_entry_type_invalid = time_entry_types :one
+    @time_entry_type_valid = time_entry_types :three
   end
 
   test "correct String representation" do
@@ -14,5 +14,9 @@ class TimeEntryTypeTest < ActiveSupport::TestCase
     assert_equal @time_entry_type_valid.description, @time_entry_type_valid.to_s
   end
 
+  test "return appropriate array for Selection" do
+    expected_array = [[@time_entry_type_valid,@time_entry_type_valid.id]]
+    assert_equal expected_array, TimeEntryType.for_select
+  end
   
 end
