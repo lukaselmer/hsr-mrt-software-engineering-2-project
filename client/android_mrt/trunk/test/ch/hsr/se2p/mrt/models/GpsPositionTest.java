7c4018d513b407b4e1333d9dd30f9719f96c8159
Delia Treichler <delia.teichler@elmer.ch>
2011-05-10 19:51:51 +0000

    refs #363

--- client/android_mrt/trunk/test/ch/hsr/se2p/mrt/models/GpsPositionTest.java
+++ client/android_mrt/trunk/test/ch/hsr/se2p/mrt/models/GpsPositionTest.java
@@ -3,16 +3,19 @@ package ch.hsr.se2p.mrt.models;
 import android.test.AndroidTestCase;
 
 public class GpsPositionTest extends AndroidTestCase {
-	private GpsPosition positionB, positionZ;
+	private GpsPosition positionA, positionB, positionZ;
 	
 	@Override
 	protected void setUp() throws Exception {
-		super.setUp();
+		super.setUp();		
+		positionA = new GpsPosition(46.949720, 7.439440);
 		positionB = new GpsPosition(46.949722, 7.439444);
 		positionZ = new GpsPosition(47.377778, 8.540278);
 	}
 	
 	public void testDistanceTo() {
-		assertEquals(95800, positionB.distanceTo(positionZ), 100);
+		assertEquals(0.0, positionA.distanceTo(positionA));
+		assertEquals(1.0, positionA.distanceTo(positionB), 1);
+		assertEquals(95800.0, positionB.distanceTo(positionZ), 100);
 	}
 }
