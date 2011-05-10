e9e648d207ca2afbf13923b8c4375e91cec49e15
Remo Waltenspuel <remo.waltenspuel@elmermx.ch>
2011-05-10 15:20:30 +0000

    refs #369

--- server/trunk/test/unit/time_entry_test.rb
+++ server/trunk/test/unit/time_entry_test.rb
@@ -21,4 +21,11 @@ class TimeEntryTest < ActiveSupport::TestCase
     invalid_record = TimeEntry.new(:description => "Invalid Entry",:time_start => 1.hour.ago)
     assert !invalid_record.valid?
   end
+
+#  test "correct duration of time_entry" do
+#
+##    TODO expected Wert überarbeiten
+#    assert_equal Thu Jan 01 07:46:24 UTC 1970, @time_entry.duration
+#
+#  end
 end
