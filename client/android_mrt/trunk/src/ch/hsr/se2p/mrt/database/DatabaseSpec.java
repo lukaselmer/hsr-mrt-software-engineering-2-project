2f74471bcef6b6cab45b8928f036d3f1d5479d0d
Lukas Elmer <lukas.elmer@gmail.com>
2011-05-16 23:38:53 +0000

    refactoring

--- client/android_mrt/trunk/src/ch/hsr/se2p/mrt/database/DatabaseSpec.java
+++ client/android_mrt/trunk/src/ch/hsr/se2p/mrt/database/DatabaseSpec.java
@@ -5,8 +5,8 @@ import ch.hsr.se2p.mrt.models.GpsPosition;
 import ch.hsr.se2p.mrt.models.TimeEntry;
 import ch.hsr.se2p.mrt.models.TimeEntryType;
 
-class DatabaseSpec {
-	protected static final String DATABASE_NAME = "mrt.db";
-	protected static final int DATABASE_VERSION = 8;
-	static final Class<?> MODEL_CLASSES[] = { TimeEntry.class, Customer.class, TimeEntryType.class, GpsPosition.class };
+public class DatabaseSpec {
+	public static final String DATABASE_NAME = "mrt.db";
+	public static final int DATABASE_VERSION = 8;
+	public static final Class<?> MODEL_CLASSES[] = { TimeEntry.class, Customer.class, TimeEntryType.class, GpsPosition.class };
 }
