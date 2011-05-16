d1468b5a246722c96230767c2d8a5c689901379d
Delia Treichler <delia.teichler@elmer.ch>
2011-05-16 09:04:18 +0000

    refs #375

--- client/android_mrt/trunk/src/ch/hsr/se2p/mrt/activities/LocationListenerAdapter.java
+++ client/android_mrt/trunk/src/ch/hsr/se2p/mrt/activities/LocationListenerAdapter.java
@@ -4,6 +4,11 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
 
+/**
+ * A Null-Object. An adapter to simplify location listeners that do not need all events defined on the LocationListener interface.
+ *
+ */
+
 class LocationListenerAdapter implements LocationListener {
 	@Override
 	public void onLocationChanged(Location location) {
