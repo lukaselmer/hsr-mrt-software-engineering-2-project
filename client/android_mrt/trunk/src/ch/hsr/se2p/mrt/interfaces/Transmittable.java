2c210148d03393a88f9eb4859570a92264383481
Delia Treichler <delia.teichler@elmer.ch>
2011-05-16 11:01:51 +0000

    refs #375

--- client/android_mrt/trunk/src/ch/hsr/se2p/mrt/interfaces/Transmittable.java
+++ client/android_mrt/trunk/src/ch/hsr/se2p/mrt/interfaces/Transmittable.java
@@ -4,14 +4,22 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * Confirms a time entry on the server. Makes sure, that object is transmitted successfully and can be removed on client.
+ * Enables the implementing class to transfer objects to the server. Makes sure, that the object is saved only once on the server.
  */
 public interface Transmittable {
+	/**
+	 * Creates a new JSONObject, initializes it
+	 * @return The newly created object
+	 */
 	public JSONObject toJSONObject();
 
+	/**
+	 * @return Successfully transmitted.
+	 */
 	public boolean isTransmitted();
 
 	/**
+	 * Compares the hashcodes on the client to the server side. If they match, the hashcode on the server is removed. Additionally the Id on the server is set.
 	 * @param jsonObject
 	 *            the object obtained by the request
 	 * @return whether response was valid or not
@@ -19,12 +27,3 @@ public interface Transmittable {
 	 */
 	public boolean processTransmission(JSONObject jsonObject) throws JSONException;
 }
-
-/**
-4.1.4.3	Transmittable
-Das Interfaces Transmittable ermöglicht es den implementierenden Klassen Objekte an den Server zu schicken. Zudem wird sichergestellt, dass Objekte nicht mehr als einmal auf dem Server gespeichert werden.
-Methode	Beschreibung
-toJSONObject()	Erstellt und ein neues JSONObject und setzt dort die jeweiligen Attribute der implementierenden Klasse. Danach wird dieses zurück gegeben.
-isTransmitted()	Vergleicht die gespeicherte ID des 
-processTransmission(JSONObject jsonObject)	Durch das erhaltene JSONObject wird der Hashcode verglichen und die ID gesetzt. Stimmt der Hashcode auf Server und Client Seite überein, wird dieser danach serverseitig entfernt. Es wird zurückgegeben ob dies erfolgreich war.
-*/
