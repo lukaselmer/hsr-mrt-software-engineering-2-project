98bef0fee928395a87f863d9e0cc43e36e78eb29
Lukas Elmer <lukas.elmer@gmail.com>
2011-05-30 03:32:08 +0000

    refactoring

--- server/trunk/app/helpers/application_helper.rb
+++ server/trunk/app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def form_error_messages f
     f.error_messages :object_name => f.object.class.model_name.human
   end
+  
+  def fl; '<div class="fl">' + yield + '</div>'; end
+  def fr; '<div class="fr">' + yield + '</div>'; end
+  def clearer; '<div class="clearer"></div>'; end
 end
