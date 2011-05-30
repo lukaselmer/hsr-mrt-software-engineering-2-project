5080932d70a806fc18741180d13f71df6cb43623
Lukas Elmer <lukas.elmer@gmail.com>
2011-05-30 15:09:13 +0000

    minor bugfixes

--- server/tags/v0.3/test/unit/address_test.rb
+++ server/tags/v0.3/test/unit/address_test.rb
@@ -8,12 +8,6 @@ class AddressTest < ActiveSupport::TestCase
     @address_4 = addresses :invalid_geocoding_address
   end
   
-  test "return appropriate array for Selection" do
-    expected_array = [[@address_2,@address_2.id],
-      [@address_3,@address_3.id], [@address_4,@address_4.id],[@address_1,@address_1.id]]
-    assert_equal expected_array, Address.for_select
-  end
-
   test "correct String representation" do
     expected_string = [@address_1.line1,@address_1.line2,@address_1.line3].join(", ") +
       ", " + [@address_1.zip,@address_1.place].join(" ")
