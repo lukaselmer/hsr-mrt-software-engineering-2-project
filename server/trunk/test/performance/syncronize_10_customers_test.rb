db7cff119fdcd70b386e9fe719770cd73c3a17d6
Diego Steiner <diego.steiner@renuo.ch>
2011-05-30 13:59:56 +0000

    refs #117

--- server/trunk/test/performance/syncronize_10_customers_test.rb
+++ server/trunk/test/performance/syncronize_10_customers_test.rb
@@ -4,11 +4,14 @@ require 'rails/performance_test_help'
 class Synchronize10CustomersTest < ActionDispatch::PerformanceTest
   def setup
     sign_in
-	10.times do
-	  # TODO: Create a customer
-	end
+    @customers = []
+    address = Address.create(:line1 => "Oberseestrasse 10", :zip => "8640", :place => "Rapperswil")
+
+    10.times do
+      @customers += Customer.create(:first_name => "Thomas", :last_name => "Tester", :phone => "0441231212", :address => address)
+    end
   end
-  
+
   def sign_in
     post_via_redirect "/users/sign_in", "user[email]" => "field_worker@mrt.ch", "user[password]" => "mrt"
     assert_equal "/", path
