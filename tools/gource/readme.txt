da76c8233874c254decc4c744cf54c41a62eb958
Lukas Elmer <lukas.elmer@gmail.com>
2011-05-31 11:40:23 +0000

    add tools and verzeichnis

---
+++ tools/gource/readme.txt
@@ -0,0 +1,3 @@
+Für Gource wird Java 1.6 benötigt. Um Videos zu exportieren wird zusätzlich ffmpeg benötigt.
+
+Weiter Informationen unter http://code.google.com/p/gource/
--- tools/gource/run.bat
+++ tools/gource/run.bat
@@ -1,4 +1,4 @@
-
+: Author: Lukas Elmer, 2011
 ECHO OFF
 CLS
 :MENU
@@ -25,24 +25,24 @@ GOTO MENU
 
 :SLOW
 cd gource-0.32.win32/
-gource.exe --user-scale 2 --highlight-users --title "MRT - Mobile Reporting Tool" --logo userdata/logo.png --user-image-dir userdata/avatars/ --file-idle-time 0 --max-files 0 --key ../../
+gource.exe --user-scale 2 --highlight-users --title "MRT - Mobile Reporting Tool" --logo userdata/logo.png --user-image-dir userdata/avatars/ --file-idle-time 0 --max-files 0 --key ../../../
 cd ../
 GOTO MENU
 
 :MEDIUM
 cd gource-0.32.win32/
-gource.exe --user-scale 2 --highlight-users --title "MRT - Mobile Reporting Tool" --logo userdata/logo.png --user-image-dir userdata/avatars/ --file-idle-time 0 --max-file-lag 1 --max-files 0 --key -s 0.3 -a 0.1  ../../
+gource.exe --user-scale 2 --highlight-users --title "MRT - Mobile Reporting Tool" --logo userdata/logo.png --user-image-dir userdata/avatars/ --file-idle-time 0 --max-file-lag 1 --max-files 0 --key -s 0.3 -a 0.1  ../../../
 cd ../
 GOTO MENU
 
 :FAST
 cd gource-0.32.win32/
-gource.exe --user-scale 2 --highlight-users  --title "MRT - Mobile Reporting Tool" --logo userdata/logo.png --user-image-dir userdata/avatars/ --file-idle-time 0 --max-file-lag 0.1 --max-files 0 --key -s 0.03 -a 0.01 ../../
+gource.exe --user-scale 2 --highlight-users  --title "MRT - Mobile Reporting Tool" --logo userdata/logo.png --user-image-dir userdata/avatars/ --file-idle-time 0 --max-file-lag 0.1 --max-files 0 --key -s 0.03 -a 0.01 ../../../
 GOTO MENU
 
 :EXPORT_SLOW
 cd gource-0.32.win32/
-gource.exe --user-scale 2 --highlight-users --title "MRT - Mobile Reporting Tool" --logo userdata/logo.png --user-image-dir userdata/avatars/ --file-idle-time 0 --max-files 0 --key ../../ -o out/ppm_slow.out
+gource.exe --user-scale 2 --highlight-users --title "MRT - Mobile Reporting Tool" --logo userdata/logo.png --user-image-dir userdata/avatars/ --file-idle-time 0 --max-files 0 --key ../../../ -o out/ppm_slow.out
 ffmpeg -y -b 500000K -r 30 -f image2pipe -vcodec ppm -i out/ppm_slow.out -vcodec libx264 -threads 0 out/gource.slow.x264.avi
 :rm ppm_slow.out
 cd ../
@@ -50,16 +50,13 @@ GOTO MENU
 
 :EXPORT_FAST
 cd gource-0.32.win32/
-gource.exe --user-scale 2 --highlight-users  --title "MRT - Mobile Reporting Tool" --logo userdata/logo.png --user-image-dir userdata/avatars/ --file-idle-time 0 --max-file-lag 0.1 --max-files 0 --key -s 0.03 -a 0.01 ../../ -o out/ppm_fast.out
+gource.exe --user-scale 2 --highlight-users  --title "MRT - Mobile Reporting Tool" --logo userdata/logo.png --user-image-dir userdata/avatars/ --file-idle-time 0 --max-file-lag 0.1 --max-files 0 --key -s 0.03 -a 0.01 ../../../ -o out/ppm_fast.out
 ffmpeg -y -b 500000K -r 30 -f image2pipe -vcodec ppm -i out/ppm_fast.out -vcodec libx264 -threads 0 out/gource.fast.x264.avi
 :rm ppm_fast.out
 cd ../
 GOTO MENU
 
-:EXPORT
 :ffmpeg -y -b 3000K -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -vpre slow -threads 0 gource.mp4
 :ffmpeg -y -b 3000K -r 60 -f image2pipe -vcodec ppm -i ppm.out -vcodec libx264 -fpre "C:\\ffmpeg\\presets\\libx264-slow.ffpreset" -threads 0 gource.x264.avi
 
-
-:END
 cls
